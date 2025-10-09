4d9f49384a0359fee2b2b0cc2d2ac942267675b3
P.Melnik <melnikpa91@gmail.com>
2025-10-09 21:00:28 +0300

    merge two sorted arrays num 88 + optimized + tests

--- src/main/java/pmelnik/local/leetcode_easy/MergeSortedArrayNum88.java
+++ src/main/java/pmelnik/local/leetcode_easy/MergeSortedArrayNum88.java
@@ -2,8 +2,55 @@ package pmelnik.local.leetcode_easy;
 
 public class MergeSortedArrayNum88 {
 
+    //my solution
+    //O(n2) time & O(1) space
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+        if (n == 0) {
+            return;
+        }
+
+        for (int i = 0; i < m; i++) {
+
+            if (nums1[i] > nums2[0]) {
+                int temp = nums1[i];
+                nums1[i] = nums2[0];
+                nums2[0] = temp;
+
+                int firstElement = nums2[0];
+                int j = 1;
+
+                while (j < n && nums2[j] < firstElement) {
+                    nums2[j - 1] = nums2[j];
+                    j++;
+                }
+                nums2[j - 1] = firstElement;
+            }
+        }
+
+        for (int j = m, i = 0; j < m + n; j++, i++) {
+            nums1[j] = nums2[i];
+        }
+    }
+
+    //additional and better solution
+    //O(m+n) time + O(1) space
+    public void optimizedMerge(int[] nums1, int m, int[] nums2, int n) {
+        int nums1end = m - 1;
+        int nums2end = n - 1;
+        int end = m + n - 1;
+
+        //идем с конца и заполняем наибольшими элементами
+        while (nums2end >= 0) {
+            if (nums1end >= 0 && nums1[nums1end] > nums2[nums2end]) {
+                nums1[end] = nums1[nums1end];
+                nums1end--;
+            } else {
+                nums1[end] = nums2[nums2end];
+                nums2end--;
+            }
+            end--;
+        }
     }
 
 }
--- src/test/java/pmelnik/local/leetcode_easy/MergeSortedArrayNum88Test.java
+++ src/test/java/pmelnik/local/leetcode_easy/MergeSortedArrayNum88Test.java
@@ -2,12 +2,51 @@ package pmelnik.local.leetcode_easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MergeSortedArrayNum88Test {
 
+    MergeSortedArrayNum88 sut = new MergeSortedArrayNum88();
+
     @Test
     void merge() {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        //expected output [1,2,2,3,5,6]
+
+        sut.merge(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+
+        assertEquals(1, nums1[0]);
+        assertEquals(2, nums1[1]);
+        assertEquals(2, nums1[2]);
+        assertEquals(3, nums1[3]);
+        assertEquals(5, nums1[4]);
+        assertEquals(6, nums1[5]);
 
     }
+
+    @Test
+    void merge2() {
+        int[] nums1 = {0};
+        int m = 0;
+        int[] nums2 = {1};
+        int n = 1;
+
+        //expected output [1]
+
+        sut.merge(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+
+        assertEquals(1, nums1[0]);
+
+    }
+
 }
