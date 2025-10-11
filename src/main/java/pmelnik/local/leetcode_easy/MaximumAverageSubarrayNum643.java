008597781e3005de4c928e821e11d9433625b557
P.Melnik <melnikpa91@gmail.com>
2025-10-12 02:11:06 +0300

    maximum average subarray number 643 O(n*k) complexity + tests

--- src/main/java/pmelnik/local/leetcode_easy/MaximumAverageSubarrayNum643.java
+++ src/main/java/pmelnik/local/leetcode_easy/MaximumAverageSubarrayNum643.java
@@ -1,11 +1,11 @@
 package pmelnik.local.leetcode_easy;
 
+
 public class MaximumAverageSubarrayNum643 {
 
+    // Не очень хорошее решение с O(n * k) по времени
     public double findMaxAverage(int[] nums, int k) {
-
-        double maxResult = Double.MIN_VALUE;
-
+        double maxResult = -Double.MAX_VALUE;
         for (int i = 0; i <= nums.length - k; i++) {
             int currentSum = 0;
             double currentResult;
@@ -16,6 +16,13 @@ public class MaximumAverageSubarrayNum643 {
             if (currentResult > maxResult) maxResult = currentResult;
         }
         return maxResult;
+    }
+
+    // решение в стиле sliding window
+    // Временаня O(n)
+    public double optimizedFindMaxAverage(int[] nums, int k) {
+        double maxResult = -Double.MAX_VALUE;
+        return  maxResult;
     }
 
 }
