841ceb8a882911584f6c73edeefc1a6960591b6b
P.Melnik <melnikpa91@gmail.com>
2025-10-17 04:13:17 +0300

    palindrome number number 9 + tests

--- src/main/java/pmelnik/local/leetcode_easy/PalindromeNumberNum9.java
+++ src/main/java/pmelnik/local/leetcode_easy/PalindromeNumberNum9.java
@@ -1,9 +1,19 @@
 package pmelnik.local.leetcode_easy;
 
+/**
+ * Time O(n)
+ * Space O(1)
+ */
 public class PalindromeNumberNum9 {
 
     public boolean isPalindrome(int x) {
-
+        String string = String.valueOf(x);
+        for (int i = 0, j = string.length() - 1; i < string.length() / 2; i++, j--) {
+            if (string.charAt(i) != string.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- src/test/java/pmelnik/local/leetcode_easy/PalindromeNumberNum9Test.java
+++ src/test/java/pmelnik/local/leetcode_easy/PalindromeNumberNum9Test.java
@@ -14,4 +14,19 @@ class PalindromeNumberNum9Test {
         boolean res = sut.isPalindrome(input);
         assertTrue(res);
     }
+
+    @Test
+    void isPalindrome2() {
+        int input = -121;
+        boolean res = sut.isPalindrome(input);
+        assertFalse(res);
+    }
+
+    @Test
+    void isPalindrome3() {
+        int input = 10;
+        boolean res = sut.isPalindrome(input);
+        assertFalse(res);
+    }
+
 }
