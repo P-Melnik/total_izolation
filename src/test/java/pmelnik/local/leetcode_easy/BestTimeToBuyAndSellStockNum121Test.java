9fe114292be053d7f92b0d669edb1b7fa55d7ea6
P.Melnik <melnikpa91@gmail.com>
2025-11-25 15:19:30 +0300

    best time to buy and sell stock tests added

--- src/test/java/pmelnik/local/leetcode_easy/BestTimeToBuyAndSellStockNum121Test.java
+++ src/test/java/pmelnik/local/leetcode_easy/BestTimeToBuyAndSellStockNum121Test.java
@@ -9,11 +9,30 @@ class BestTimeToBuyAndSellStockNum121Test {
     BestTimeToBuyAndSellStockNum121 sut = new BestTimeToBuyAndSellStockNum121();
 
     @Test
-    void maxProfit() {
+    void maxProfitFive() {
         int[] prices = {7,1,5,3,6,4};
 
         int res = sut.maxProfit(prices);
 
         assertEquals(5, res);
     }
+
+    @Test
+    void maxProfitZero() {
+        int[] prices = {7,6,4,3,1};
+
+        int res = sut.maxProfit(prices);
+
+        assertEquals(0, res);
+    }
+
+    @Test
+    void maxProfitHundred() {
+        int[] prices = {15, 14, 555, 90, 73, 13, 89, 14, 99, 113, 102, 11, 111};
+
+        int res = sut.maxProfit(prices);
+
+        assertEquals(100, res);
+    }
+
 }
