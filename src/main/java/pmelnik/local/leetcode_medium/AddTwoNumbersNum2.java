5898b80cd290bee3d4c37a15ba209374900899a5
P.Melnik <melnikpa91@gmail.com>
2025-12-10 20:40:05 +0300

    add two numbers num 2 recursive solution

--- src/main/java/pmelnik/local/leetcode_medium/AddTwoNumbersNum2.java
+++ src/main/java/pmelnik/local/leetcode_medium/AddTwoNumbersNum2.java
@@ -52,4 +52,29 @@ public class AddTwoNumbersNum2 {
         return dummyHead.next;
     }
 
+    public ListNode addTwoNumbersRecursive(ListNode l1, ListNode l2) {
+        return callRecursiveAddTwoNimbers(l1, l2, 0);
+    }
+
+    private ListNode callRecursiveAddTwoNimbers(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
+        }
+
+        int sum = carry;
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        ListNode node = new ListNode(sum % 10);
+        node.next = callRecursiveAddTwoNimbers(l1, l2, sum / 10);
+
+        return node;
+    }
+
 }
