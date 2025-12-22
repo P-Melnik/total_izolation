fc69b8268e5c78f41b2aa8ac465354f9f3e88a74
Pavel M <melnikpa91@gmail.com>
2025-12-23 01:15:39 +0300

    pascal's triangle num 118 starting tests

---
+++ src/test/java/pmelnik/local/leetcode_easy/PascalsTriangleNum118Test.java
@@ -0,0 +1,23 @@
+package pmelnik.local.leetcode_easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PascalsTriangleNum118Test {
+
+    PascalsTriangleNum118 sut = new PascalsTriangleNum118();
+
+    @Test
+    void generate() {
+        int input = 5;
+
+        List<List<Integer>> result = List.of(
+                List.of(1),
+                List.of(1, 1)
+                //todo: finish this test data
+        );
+    }
+}
