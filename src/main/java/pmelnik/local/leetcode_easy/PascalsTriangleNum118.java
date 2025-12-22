46e91eb3e407d46dbdb4818f8b60492c2c7ef25f
Pavel M <melnikpa91@gmail.com>
2025-12-22 23:14:03 +0300

    Pascal's triangle number 118 init

---
+++ src/main/java/pmelnik/local/leetcode_easy/PascalsTriangleNum118.java
@@ -0,0 +1,15 @@
+package pmelnik.local.leetcode_easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ */
+public class PascalsTriangleNum118 {
+
+    public List<List<Integer>> generate(int numRows) {
+        return new ArrayList<>();
+    }
+}
