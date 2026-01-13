f18537d6a6ebb82b925f1f902f90266df7705c30
Pavel M <melnikpa91@gmail.com>
2026-01-13 13:24:07 +0300

    Maximize Number tests

--- src/test/java/pmelnik/local/testTaskAICoachQuestionnaire/MaximizeNumberTest.java
+++ src/test/java/pmelnik/local/testTaskAICoachQuestionnaire/MaximizeNumberTest.java
@@ -1,5 +1,6 @@
 package pmelnik.local.testTaskAICoachQuestionnaire;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,7 +11,57 @@ class MaximizeNumberTest {
 
 
     @Test
-    void maximizeA_test1() {
+    void maximizeA_basic() {
 
+        String a = "1234";
+        String b = "5987";
+
+        String res = sut.maximizeA(a, b);
+
+        Assertions.assertEquals("9875", res);
+    }
+
+    @Test
+    void maximizeA_basic2() {
+
+        String a = "1234";
+        String b = "9999";
+
+        String res = sut.maximizeA(a, b);
+
+        Assertions.assertEquals("9999", res);
+    }
+
+    @Test
+    void maximizeA_noSubstitutions() {
+
+        String a = "4321";
+        String b = "123";
+
+        String res = sut.maximizeA(a, b);
+
+        Assertions.assertEquals("4332", res);
+    }
+
+    @Test
+    void maximizeA_simpleReplacement() {
+
+        String a = "1000";
+        String b = "9";
+
+        String res = sut.maximizeA(a, b);
+
+        Assertions.assertEquals("9000", res);
+    }
+
+    @Test
+    void maximizeA_emptyB() {
+
+        String a = "1234";
+        String b = "";
+
+        String res = sut.maximizeA(a, b);
+
+        Assertions.assertEquals("1234", res);
     }
 }
