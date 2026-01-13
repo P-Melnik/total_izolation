e4d1a21614357daeb56282603b6adf5c293520af
Pavel M <melnikpa91@gmail.com>
2026-01-13 13:15:50 +0300

    Maximize Number solution

---
+++ src/test/java/pmelnik/local/testTaskAICoachQuestionnaire/MaximizeNumberTest.java
@@ -0,0 +1,16 @@
+package pmelnik.local.testTaskAICoachQuestionnaire;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximizeNumberTest {
+
+    MaximizeNumber sut = new MaximizeNumber();
+
+
+    @Test
+    void maximizeA_test1() {
+
+    }
+}
