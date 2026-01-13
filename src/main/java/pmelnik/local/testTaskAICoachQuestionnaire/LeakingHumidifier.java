69a72c008922d9dd83b3c5eac8ea8266fb77150a
Pavel M <melnikpa91@gmail.com>
2026-01-13 12:23:07 +0300

    Leaking Humidifier task init

---
+++ src/main/java/pmelnik/local/testTaskAICoachQuestionnaire/LeakingHumidifier.java
@@ -0,0 +1,8 @@
+package pmelnik.local.testTaskAICoachQuestionnaire;
+
+public class LeakingHumidifier {
+
+    public int calculateAmountOfWaterAfterAddingWater(String input) {
+        return 0;
+    }
+}
