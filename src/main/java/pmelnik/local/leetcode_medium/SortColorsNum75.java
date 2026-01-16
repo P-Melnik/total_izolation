c5c4e5ffdc0b955480d877cb368895b6657aac87
Pavel M <melnikpa91@gmail.com>
2026-01-16 17:06:47 +0300

    sort colors number 75 solution

--- src/main/java/pmelnik/local/leetcode_medium/SortColorsNum75.java
+++ src/main/java/pmelnik/local/leetcode_medium/SortColorsNum75.java
@@ -3,7 +3,32 @@ package pmelnik.local.leetcode_medium;
 public class SortColorsNum75 {
 
     public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
 
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Меняем nums[mid] и nums[low]
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                // Просто пропускаем
+                mid++;
+            } else { // nums[mid] == 2
+                // Меняем nums[mid] и nums[high]
+                swap(nums, mid, high);
+                high--;
+                // mid НЕ увеличиваем, так как нужно проверить новый элемент на позиции mid
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 }
