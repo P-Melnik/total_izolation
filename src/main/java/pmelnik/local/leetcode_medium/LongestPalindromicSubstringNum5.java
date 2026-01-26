f1a5bada2115540149410d5ea56c4dd87f0ba022
Pavel M <melnikpa91@gmail.com>
2026-01-26 19:00:32 +0300

    longest palindromic substring comments added

--- src/main/java/pmelnik/local/leetcode_medium/LongestPalindromicSubstringNum5.java
+++ src/main/java/pmelnik/local/leetcode_medium/LongestPalindromicSubstringNum5.java
@@ -1,8 +1,18 @@
 package pmelnik.local.leetcode_medium;
 
+/**
+ * Краткое описание решения
+ * Используем подход "расширение от центра".
+ * Для каждого символа (и для каждой пары символов) проверяем,
+ * можно ли расширить палиндром в обе стороны.
+ * ---
+ * Анализ сложности
+ * Время: O(n²) - для каждого центра (2n-1 центров) расширяем потенциально до O(n)
+ * Память: O(1) - только константная дополнительная память
+ */
 public class LongestPalindromicSubstringNum5 {
 
-    //"babad"
+
     public String longestPalindromicSubstring(String s) {
 
         for (int i = 0, j = s.length() - 1; i < s.length()/2; i++, j--) {
