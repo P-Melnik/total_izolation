314b35acc3194a42475d13a0b08160a60592e222
Pavel M <melnikpa91@gmail.com>
2026-01-30 13:36:15 +0300

    reverse integer num 7 tests prepared

---
+++ src/test/java/pmelnik/local/leetcode_medium/ReverseIntegerNum7Test.java
@@ -0,0 +1,35 @@
+package pmelnik.local.leetcode_medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseIntegerNum7Test {
+
+    ReverseIntegerNum7 sut = new ReverseIntegerNum7();
+
+    @Test
+    void reverse_1() {
+        var input = 123;
+        int res = sut.reverse(input);
+
+        assertEquals(312, res);
+    }
+
+    @Test
+    void reverse_2() {
+        var input = -123;
+        int res = sut.reverse(input);
+
+        assertEquals(-321, res);
+    }
+
+    @Test
+    void reverse_3() {
+        var input = 120;
+        int res = sut.reverse(input);
+
+        assertEquals(12, res);
+    }
+
+}
